93c326f49e892e4590cbbd606cbcf139353dccc8
LexusPW <magicwinterdreams2191@gmail.com>
2025-10-17 19:50:08 -0400

    Add files via upload

--- recipes/index.php
+++ recipes/index.php
@@ -283,6 +283,22 @@
 					</div>
 				</div>
 
+				<div class="stickyNote flex">
+					<div class="video">
+						<iframe src="https://www.youtube.com/embed/VNkmZedklwI?si=RDT_2aqb_PaNs2CH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+						<div class="border"></div>
+					</div>
+					<div class="info">
+						<h3>Girl's Night</h3>
+						<p><i>Noisette organizes a girls' night with Fake Peppino and Vigilante.</i></p>
+						<p>Thumbmail by MakeeDoodles.</p>
+						<p>Animatic by MakeeDoodles.</p>
+						<p>Written by Skullduggery.</p>
+						<p>Edited by SkytheVirus.</p>
+						<p>Music composed by ThesisFi.</p>
+					</div>
+				</div>
+
 			</div>
 			
 		</main>
